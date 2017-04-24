a624dbc201f5aa608d21dee9ece8429812c81002
Archerkiki <641363674@qq.com>
2017-04-24 22:53:18 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "include/sort.h"
+
+int main() {
+        int a[10]={23,4,6,28,36,23,1};
+        int n=7;
+        maopao(a,n);
+        D_insert(a,n);
+        choose_sort(a,n);
+        return 0;
+}
